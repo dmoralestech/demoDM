b0e88396053d5786d9637bcf0b9eb29aaa88e0c2
darwinmorales <darwin.morales.tech@gmail.com>
2017-06-06 21:19:04 +1000

    First Commit

---
+++ src/main/java/com/darwin/demoDM/DemoDmApplication.java
@@ -0,0 +1,12 @@
+package com.darwin.demoDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDmApplication.class, args);
+	}
+}
