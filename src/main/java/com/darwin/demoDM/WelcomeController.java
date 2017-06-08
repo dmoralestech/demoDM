742552128776009fb0b01036d680bd183816a6a3
darwinmorales <darwin.morales.tech@gmail.com>
2017-06-08 21:21:52 +1000

    Hello World

---
+++ src/main/java/com/darwin/demoDM/WelcomeController.java
@@ -0,0 +1,25 @@
+package com.darwin.demoDM;
+
+/**
+ * Created by darwinmorales on 8/06/2017.
+ */
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+    // inject via application.properties
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
+
+}
